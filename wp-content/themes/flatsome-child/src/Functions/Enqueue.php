ef4b4a53acc29c7f2721262a6f8163315b7f424a
quyle91 <quylv.dsth@gmail.com>
2023-12-01 06:08:58 +0700

    s

--- wp-content/themes/flatsome-child/src/Functions/Enqueue.php
+++ wp-content/themes/flatsome-child/src/Functions/Enqueue.php
@@ -4,25 +4,14 @@ class Enqueue {
     
     function __construct() {
         add_action( 'wp_enqueue_scripts', function(){
-            // CSS
-            wp_enqueue_style( 'sconnect-style', Sconnect_Url."/assets/css/flatsome-main.css", [], null, 'all' );
-            wp_enqueue_style( 'sconnect-custom-classes', Sconnect_Url."/assets/css/custom-classes.css", [], null, 'all' );
-            wp_enqueue_style( 'sconnect-elements', Sconnect_Url."/assets/css/flatsome-elements.css", [], null, 'all' );
-            wp_enqueue_style( 'sconnect-woo-main', Sconnect_Url."/assets/css/woocommerce-main.css", [], null, 'all' );
-            wp_enqueue_style( 'sconnect-woo-cart-checkout', Sconnect_Url."/assets/css/woocommerce-cart-checkout.css", [], null, 'all' );
-            wp_enqueue_style( 'sconnect-single-product', Sconnect_Url."/assets/css/woocommerce-single-product.css", [], null, 'all' );
-
-            $is_quy = false;
-            if(is_user_logged_in() and wp_get_current_user()->data->user_login == 'quyle'){
-                $is_quy = true;
-            }
             
-            if(!$is_quy){
+            wp_enqueue_style( 'sconnect-base', Sconnect_Url."/assets/css/base.css", [], null, 'all' );
+            wp_enqueue_style( 'sconnect-base-woo', Sconnect_Url."/assets/css/base-woo.css", [], null, 'all' );
+
+            if(!isset($_GET['dev'])){
                 wp_enqueue_style( 'tai-custom-style', Sconnect_Url."/assets/css/tai-custom-style.css", [], null, 'all' );
                 wp_enqueue_style( 'tai-base-css', Sconnect_Url."/assets/css/tai-base-css.css", [], null, 'all' );
             }
-
-            // JS - GLobal Js
             
         } );
     }
