f7d7eaa762b9cff3986e2ec2742141a45bbf60f8
quyle91 <quylv.dsth@gmail.com>
2023-12-01 18:08:57 +0700

    CEO

--- wp-content/themes/flatsome-child/src/Functions/Enqueue.php
+++ wp-content/themes/flatsome-child/src/Functions/Enqueue.php
@@ -9,8 +9,8 @@ class Enqueue {
             wp_enqueue_style( 'sconnect-base-woo', Sconnect_Url."/assets/css/base-woo.css", [], null, 'all' );
 
             if(!isset($_GET['dev'])){
-                wp_enqueue_style( 'tai-custom-style', Sconnect_Url."/assets/css/tai-custom-style.css", [], null, 'all' );
-                wp_enqueue_style( 'tai-base-css', Sconnect_Url."/assets/css/tai-base-css.css", [], null, 'all' );
+                wp_enqueue_style( 'tai-custom-style', Sconnect_Url."/assets/css/__tai-custom-style.css", [], null, 'all' );
+                wp_enqueue_style( 'tai-base-css', Sconnect_Url."/assets/css/__tai-base-css.css", [], null, 'all' );
             }
             
         } );
