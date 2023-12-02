60d6c9b62cdaf380b1fa517e73343a9b7073cbfe
AnhQuan <quannguyen2101999@gmail.com>
2023-12-02 16:52:16 +0700

    sua trang ve chung toi

--- wp-content/themes/flatsome-child/src/Functions/Enqueue.php
+++ wp-content/themes/flatsome-child/src/Functions/Enqueue.php
@@ -1,14 +1,5 @@
 <?php
 
-<<<<<<< Updated upstream
-            if(!isset($_GET['dev'])){
-                wp_enqueue_style( 'tai-custom-style', Sconnect_Url."/assets/css/__tai-custom-style.css", [], null, 'all' );
-                wp_enqueue_style( 'tai-base-css', Sconnect_Url."/assets/css/__tai-base-css.css", [], null, 'all' );
-            }
-            
-        } );
-    }
-=======
 namespace Sconnect\Functions;
 
 class Enqueue
@@ -22,11 +13,10 @@ class Enqueue
       wp_enqueue_style('sconnect-base-woo', Sconnect_Url . "/assets/css/base-woo.css", [], null, 'all');
 
       if (!isset($_GET['dev'])) {
-        wp_enqueue_style('tai-custom-style', Sconnect_Url . "/assets/css/tai-custom-style.css", [], null, 'all');
-        wp_enqueue_style('tai-base-css', Sconnect_Url . "/assets/css/tai-base-css.css", [], null, 'all');
+        wp_enqueue_style('tai-custom-style', Sconnect_Url . "/assets/css/__tai-custom-style.css", [], null, 'all');
+        wp_enqueue_style('tai-base-css', Sconnect_Url . "/assets/css/__tai-base-css.css", [], null, 'all');
         wp_enqueue_style('quan-css', Sconnect_Url . "/assets/css/quan.css", [], null, 'all');
       }
     });
   }
->>>>>>> Stashed changes
 }
