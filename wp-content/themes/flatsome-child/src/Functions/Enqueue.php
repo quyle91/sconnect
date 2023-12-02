7b3826903bc7831da32e0a7b3f123f9d6fdc5b71
AnhQuan <quannguyen2101999@gmail.com>
2023-12-02 14:56:52 +0700

    backup code of quan

--- wp-content/themes/flatsome-child/src/Functions/Enqueue.php
+++ wp-content/themes/flatsome-child/src/Functions/Enqueue.php
@@ -1,13 +1,6 @@
 <?php
-namespace Sconnect\Functions;
-class Enqueue {
-    
-    function __construct() {
-        add_action( 'wp_enqueue_scripts', function(){
-            
-            wp_enqueue_style( 'sconnect-base', Sconnect_Url."/assets/css/base.css", [], null, 'all' );
-            wp_enqueue_style( 'sconnect-base-woo', Sconnect_Url."/assets/css/base-woo.css", [], null, 'all' );
 
+<<<<<<< Updated upstream
             if(!isset($_GET['dev'])){
                 wp_enqueue_style( 'tai-custom-style', Sconnect_Url."/assets/css/__tai-custom-style.css", [], null, 'all' );
                 wp_enqueue_style( 'tai-base-css', Sconnect_Url."/assets/css/__tai-base-css.css", [], null, 'all' );
@@ -15,4 +8,25 @@ class Enqueue {
             
         } );
     }
+=======
+namespace Sconnect\Functions;
+
+class Enqueue
+{
+
+  function __construct()
+  {
+    add_action('wp_enqueue_scripts', function () {
+
+      wp_enqueue_style('sconnect-base', Sconnect_Url . "/assets/css/base.css", [], null, 'all');
+      wp_enqueue_style('sconnect-base-woo', Sconnect_Url . "/assets/css/base-woo.css", [], null, 'all');
+
+      if (!isset($_GET['dev'])) {
+        wp_enqueue_style('tai-custom-style', Sconnect_Url . "/assets/css/tai-custom-style.css", [], null, 'all');
+        wp_enqueue_style('tai-base-css', Sconnect_Url . "/assets/css/tai-base-css.css", [], null, 'all');
+        wp_enqueue_style('quan-css', Sconnect_Url . "/assets/css/quan.css", [], null, 'all');
+      }
+    });
+  }
+>>>>>>> Stashed changes
 }
